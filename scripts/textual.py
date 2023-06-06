36804c9d6ddd17666a2ecbef8c1255c9a6d34471
Sahand Nazarzadeh <Sahand.Nazarzadeh@gmail.com>
2023-06-06 21:30:49 +0330

    Update scripts: Make them more structural to increase usability of thier methods

--- scripts/textual.py
+++ scripts/textual.py
@@ -3,15 +3,19 @@ import os
 
 from src.crawler.textual.cryptonews import CryptoNewsScraper
 
+
+def crawl(source: str, selenium_driver_path: str):
+    os.environ['PATH'] += selenium_driver_path
+
+    if 'cryptonews' == source:
+        scraper = CryptoNewsScraper()
+        scraper.run()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--chrome-driver-path', action='store', type=str, required=True)
     parser.add_argument('--source', action='store', type=str, required=True)
-    parser.add_argument('--threads', action='store', type=str, required=False, default=4)
+    parser.add_argument('--selenium-driver-path', action='store', type=str, required=True)
     args = parser.parse_args()
 
-    os.environ['PATH'] += args.chrome_driver_path
-
-    if 'cryptonews' == args.source:
-        scraper = CryptoNewsScraper(threads=args.threads)
-        scraper.run()
+    crawl(source=args.source, selenium_driver_path=args.selenium_driver_path)
